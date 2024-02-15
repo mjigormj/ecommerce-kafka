348d692dc5d461062372569460ef73937aa7adc8
mjigormj <igorcruzsilva17@gmail.com>
2024-02-15 00:19:47 -0300

    Começando com o Kafka

--- src/main/java/br/com/mjigormj/ecommerce/EmailService.java
+++ src/main/java/br/com/mjigormj/ecommerce/EmailService.java
@@ -1,13 +1,19 @@
 package br.com.mjigormj.ecommerce;
 
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.util.Map;
 
 public class EmailService {
     public static void main(String[] args) {
         var emailService = new EmailService();
         try (var service = new KafkaService(EmailService.class.getSimpleName(),
                 "ECOMMERCE_SEND_EMAIL",
-                emailService::parse)) {
+                emailService::parse,
+                String.class,
+                Map.of(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName()))){
             service.run();
         }
     }
--- src/main/java/br/com/mjigormj/ecommerce/NewOrderMain.java
+++ src/main/java/br/com/mjigormj/ecommerce/NewOrderMain.java
@@ -12,9 +12,11 @@ public class NewOrderMain {
                     var userId = UUID.randomUUID().toString();
                     var orderId = UUID.randomUUID().toString();
                     var amount = new BigDecimal(Math.random() * 5000 + 1);
+
                     var order = new Order(userId, orderId, amount);
                     orderDispatcher.send("ECOMMERCE_NEW_ORDER", userId, order);
 
+//                    var email = new Email("ECOMMERCE_NEW_ORDER", "Thank you for your order!, We are processing your order!");
                     var email = "Thank you for your order!, We are processing your order!";
                     emailDispatcher.send("ECOMMERCE_SEND_EMAIL", userId, email);
                 }
